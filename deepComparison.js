8caf24c95a0f7a0e2c50ebeeaec5dad1fe5d8d98
capprimmir <capprimmir21@gmail.com>
2019-06-04 15:12:00 -0400

    add travis file to create builds

--- deepComparison.js
+++ deepComparison.js
@@ -2,22 +2,25 @@
 
 function deepEqual (item1, item2) {
   if (item1 === item2 ) return true;
-  if ((typeof item1 == 'object' && item1 != null) && 
-      (typeof item2 == 'object' && item2 != null)) {
-        //get properties of each object
-        let keys1 = Object.keys(item1), keys2 = Object.keys(item2);
+  if (typeof item1 != 'object' || item1 == null ||
+      typeof item2 != 'object' || item2 == null) return false
 
-        // check if different length
-        if (keys1.length != keys2.length) return false;
+  //get properties of each object
+  let keys1 = Object.keys(item1), keys2 = Object.keys(item2);
 
-        // check if item in one array are included in other array
-        // call the function again using recursive
-        for (key of keys1) {
-          if (!keys2.includes(key) || !deepEqual(item1[key], item2[key])) return false;
-        }
-       
+  // check if different length
+  if (keys1.length != keys2.length) return false;
+
+  // check if items in one array are included in other array
+  // call the function again using recursive
+  for (key of keys1) {
+    if (!keys2.includes(key) || !deepEqual(item1[key], item2[key])) return false;
   }
   return true;
 }
 let obj = {here: {is: "an"}, object: 2};
 console.log(deepEqual(obj, obj));
+
+console.log(deepEqual(obj, {here: 1, object: 2}));
+
+console.log(deepEqual(obj, {here: {is: "an"}, object: 2}));
